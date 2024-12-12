f538c3df48de8f63e01b6c9b932ae197d72f2313
oussama <ou.charafi@gmail.com>
2024-12-12 14:40:22 +0100

    I added the service of ticket and service gateway et discovery

---
+++ co-cinema/geteway-service/src/test/java/co/cinema/getewayservice/GetewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package co.cinema.getewayservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GetewayServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
