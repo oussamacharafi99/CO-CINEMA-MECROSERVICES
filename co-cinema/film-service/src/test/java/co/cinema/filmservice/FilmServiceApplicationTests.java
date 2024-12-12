00ee33e8c0633eaeaa404edb090ac0c2a9186f60
oussama <ou.charafi@gmail.com>
2024-12-12 14:30:57 +0100

    I added the first service of the project co-cinema

---
+++ co-cinema/film-service/src/test/java/co/cinema/filmservice/FilmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package co.cinema.filmservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FilmServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
